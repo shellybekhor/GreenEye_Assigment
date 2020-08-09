d9907fd76e7d1a1f0679bb53e37eff8625427951
ian <8shelly8@gmail.com>
2020-08-09 19:17:57 +0300

    iterate images

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,26 +1,60 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include <vector>
+#include <boost/filesystem.hpp>
 
 
-cv::Mat grayImage(cv::Mat& img)
+int grayImage(cv::Mat& img, cv::Mat& result)
 {
+
 	int h = img.rows;
 	int w = img.cols;
 	int c = img.channels();
 
-	assert(c==3);
+	if ((c != 3) or h != result.rows or w != result.cols)
+	{
+		std::cout << "invalid parameters" << std::endl;
+		return -1;
+	}
 
-	uchar* im_out = (uchar*)malloc(h * w * sizeof(uchar));
+	uchar* im_out = result.ptr<uchar>(0);
 	uchar* im_in = img.ptr<uchar>(0);
 
-	for (int i=0, p_i=0; i < h * w * c; i += c, p_i++) {
+	for (int i=0, p_i=0; i < h * w * c; i += c, p_i++)
+	{
 		im_out[p_i] =  0.299 * im_in[i] + 0.587 * im_in[i+1] + 0.114 * im_in[i+2];
 	}
 
-	return cv::Mat(h, w, CV_8UC1, im_out);
+	return 0;
 }
+
+
+int iterateImages(std::string folderName)
+{
+	int i = 0;
+    for (auto f = boost::filesystem::directory_iterator(folderName); f != boost::filesystem::directory_iterator(); f++)
+    {
+        if (!boost::filesystem::is_directory(f->path())) // eliminate directories
+        {
+            std::string fn = f->path().filename().string();
+        	std::cout << fn << std::endl;
+
+            cv::Mat image;
+            image = cv::imread(f->path().string(), cv::IMREAD_COLOR);
+            if ( !image.data )
+            {
+                printf("No image data \n");
+                return -1;
+            }
+            cv::Mat result(image.rows, image.cols, CV_8UC1);
+			grayImage(image, result);
+			cv::imwrite("image_" + std::to_string(++i) + ".jpg", result);
+        }
+        else
+            continue;
+    }
+}
+
 
 
 int main(int argc, char** argv)
@@ -30,16 +64,9 @@ int main(int argc, char** argv)
         printf("usage: DisplayImage.out <Image_Path>\n");
         return -1;
     }
-    cv::Mat image;
-    image = cv::imread( argv[1], cv::IMREAD_COLOR);
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    cv::Mat result = grayImage(image);
-    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
-    cv::imshow("Display Image", result);
-    cv::waitKey(0);
+//    std::string folder = argv[1];
+    std::string folder = "/home/ian/Documents/Shelly/opencv/DisplayImage/images/";
+    iterateImages(folder);
+
     return 0;
 }
